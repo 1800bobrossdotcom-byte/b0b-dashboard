2188b54192256284f6ef05c476e8ffe61136c9ca
1800bobrossdotcom-byte <1800bobrossdotcom@gmail.com>
2026-03-28 21:42:03 -0400

    Redirect b0b.dev and 1-800-bob-ross.com to irl.ing

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect old domains to irl.ing
+app.use((req, res, next) => {
+  const host = req.hostname;
+  if (host === 'b0b.dev' || host === 'www.b0b.dev' ||
+      host === '1-800-bob-ross.com' || host === 'www.1-800-bob-ross.com') {
+    return res.redirect(301, 'https://irl.ing' + req.originalUrl);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
